cb86f2413e35f409239add1a04cc8f208419b7c2
Petry <petry078@gmail.com>
2024-08-26 11:33:27 -0300

    hm

--- index.js
+++ index.js
@@ -8,16 +8,45 @@ const PORT = 3000
 app.use(express.json())
 
 //GET all students
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/', async (req, res) => {
+    const students = await prisma.note.findMany()
+    return res.json(students)
   })
   
 //GET student by ID
+app.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const noted = await prisma.note.findUnique({
+      where: {
+        id: Number(id),
+      },
+    })
+    return res.json(noted)
+  })
+
 //POST student
-//PUT (update whole) student
-//PATCH (update single filed) student
+app.post('/', async (req, res) => {
+    const newNoted = await prisma.note.create({
+      data: {
+        noted: req.body.noted,
+      },
+    })
+  
+    return res.json(newNoted)
+  })
+  
 //DELETE student
+  app.delete('/:id', async (req, res) => {
+    const id = req.params.id
+  
+    await prisma.note.delete({
+      where: { id: Number(id) },
+    })
+  
+    res.json(`Note ${id} deleted.`)
+  })
+
+//Server start log
 app.listen(PORT, () => {
-    console.log(`App runing on PORT ${PORT}`)
+    console.log(`App runing on http://localhost:${PORT}/`)
   })
