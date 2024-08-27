96a535f875a09009cda60f347989050b720d5b3c
petry <petry@pop-os.localdomain>
2024-08-27 03:20:37 -0300

    error handeling

--- index.js
+++ index.js
@@ -9,39 +9,52 @@ app.use(express.json())
 
 //GET all students
 app.get('/', async (req, res) => {
-    const students = await prisma.student.findMany()
-    return res.json(students)
-  })
-  
+  try {
+    const students = await prisma.student.findMany();
+    return res.status(200).json(students);
+  } catch (error) {
+    res.status(500).send("Error fetching students.");
+  }
+});
+
 //GET student by ID
 app.get('/:id', async (req, res) => {
-    const id = req.params.id
+  const id = req.params.id;
+  try {
     const student = await prisma.student.findUnique({
       where: {
         id: Number(id),
       },
-    })
-    return res.json(student)
-  })
+    });
+    return res.status(200).json(student);
+  } catch (error) {
+    res.status(500).send(`Error fetching student ${id}.`);
+  }
+});
 
 //POST student
 app.post('/', async (req, res) => {
-  const postStudent = await prisma.student.create({
-    data: {
-      name: req.body.name,
-      age: req.body.age,
-      n1: req.body.n1,
-      n2: req.body.n2,
-      teacher: req.body.teacher,
-      class: req.body.class
-    }
-  })
-  return res.json(postStudent)    
-}) 
+  try {
+    const postStudent = await prisma.student.create({
+      data: {
+        name: req.body.name,
+        age: req.body.age,
+        n1: req.body.n1,
+        n2: req.body.n2,
+        teacher: req.body.teacher,
+        class: req.body.class
+      }
+    });
+    return res.status(200).json(postStudent);
+  } catch (error) {
+    res.status(500).send("Error creating student.");
+  }
+});
 
 //PUT student
 app.put('/:id', async (req, res) => {
-    const id = req.params.id
+  const id = req.params.id;
+  try {
     const student = await prisma.student.update({
       where: { id: Number(id) },
       data: {
@@ -52,14 +65,17 @@ app.put('/:id', async (req, res) => {
         teacher: req.body.teacher,
         class: req.body.class
       }
-    })
-  
-    return res.json(student)
-  })
+    });
+    return res.status(200).json(student);
+  } catch (error) {
+    res.status(500).send(`Error updating student ${id}.`);
+  }
+});
 
 //PATCH student
 app.patch('/:id', async (req, res) => {
-    const id = req.params.id
+  const id = req.params.id;
+  try {
     const student = await prisma.student.update({
       where: { id: Number(id) },
       data: {
@@ -70,10 +86,12 @@ app.patch('/:id', async (req, res) => {
         teacher: req.body.teacher,
         class: req.body.class
       }
-    })
-
-    return res.json(student)
-  })
+    });
+    return res.status(200).json(student);
+  } catch (error) {
+    res.status(500).send(`Error patching student ${id}.`);
+  }
+});
 
 //DELETE student
 app.delete('/:id', async (req, res) => {
@@ -84,12 +102,11 @@ app.delete('/:id', async (req, res) => {
         id: Number(id),
       },
     });
-    res.status(200).send("Student ${id} deleted.");
+    res.status(200).send(`Student ${id} deleted.`);
   } catch (error) {
-    res.status(500).send("Error deleting student ${id}.");
+    res.status(500).send(`Error deleting student ${id}.`);
   }
 });
-
 //Server start log
 app.listen(PORT, () => {
     console.log(`App runing on http://localhost:${PORT}/`)
